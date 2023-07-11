376d2e9613fd2376d5907f83c25da6fbf4ab07a3
pravin82 <pravinmishra1796@gmail.com>
2023-07-11 07:46:03 +0530

    Do cargo fmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,8 +1,8 @@
 extern crate clap;
 
-use std::process::exit;
 use clap::{App, AppSettings, Arg, SubCommand};
-fn main(){
+use std::process::exit;
+fn main() {
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -11,15 +11,15 @@ fn main(){
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .setting(AppSettings::VersionlessSubcommands)
         .subcommand(
-        SubCommand::with_name("set")
-            .about("Set the value of a string key to a string")
-            .arg(Arg::with_name("KEY").help("A string key").required(true))
-            .arg(
-                Arg::with_name("VALUE")
-                    .help("The string value of the key")
-                    .required(true),
-            ),
-    )
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .arg(Arg::with_name("KEY").help("A string key").required(true))
+                .arg(
+                    Arg::with_name("VALUE")
+                        .help("The string value of the key")
+                        .required(true),
+                ),
+        )
         .subcommand(
             SubCommand::with_name("get")
                 .about("Get the string value of a given string key")
@@ -46,10 +46,4 @@ fn main(){
         }
         _ => unreachable!(),
     }
-
-
-
 }
-
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 pub struct KvStore {
-    memory_db: HashMap<String, String>
+    memory_db: HashMap<String, String>,
 }
 
 impl Default for KvStore {
@@ -11,19 +11,18 @@ impl Default for KvStore {
 }
 
 impl KvStore {
+    pub fn new() -> KvStore {
+        let memory_db = HashMap::new();
+        KvStore { memory_db }
+    }
+    pub fn set(&mut self, key: String, value: String) -> Option<String> {
+        self.memory_db.insert(key, value)
+    }
+    pub fn get(&self, key: String) -> Option<String> {
+        self.memory_db.get(&key).cloned()
+    }
 
-    pub fn new()->KvStore{
-        let  memory_db = HashMap::new();
-        KvStore{memory_db}
-    }
-    pub fn set(&mut self, key:String, value:String) -> Option<String> {
-       self.memory_db.insert(key, value)
-    }
-    pub fn get(&self,key:String) -> Option<String>{
-       self.memory_db.get(&key).cloned()
-    }
-
-    pub fn remove(&mut self, key:String) -> Option<String>{
-       self.memory_db.remove(&key)
+    pub fn remove(&mut self, key: String) -> Option<String> {
+        self.memory_db.remove(&key)
     }
 }
