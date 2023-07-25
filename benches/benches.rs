d6a8a756b5602ac6eec650ca2e964cf39d574962
pravin82 <pravinmishra1796@gmail.com>
2023-07-25 08:37:26 +0530

    Initial implementation of KvsSledEngine

---
+++ benches/benches.rs
@@ -0,0 +1,2 @@
+use criterion::{ criterion_group, criterion_main, Criterion};
+use kvs::{KvStore, KvsEngine, SledKvsEngine};
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::io::prelude::*;
 extern crate serde_json;
 use std::{fs};
 use std::io::{BufReader, SeekFrom};
+use sled::Db;
 pub use error::{KvsError,Result};
 mod error;
 mod kvs_engine;
@@ -211,6 +212,46 @@ impl KvsEngine for KvStore {
     }
 
 }
+
+pub struct SledKvsEngine{
+    db:Db
+}
+
+impl SledKvsEngine {
+    pub fn open(path: &Path) -> sled::Result<SledKvsEngine> {
+        let tree = sled::open(path).unwrap();
+        Ok(SledKvsEngine{db:tree})
+    }
+}
+
+impl KvsEngine for SledKvsEngine{
+    fn set(&mut self, key: String, value: String) -> Result<Option<String>> {
+         self.db.insert(key,sled::IVec::from(value.as_bytes()));
+        Ok(Some("SUCCESS".to_string()))
+    }
+
+    fn get(&mut self, key: String) -> Result<Option<String>> {
+       let resp =  self.db.get(key).unwrap();
+        if let Some(i) = resp {
+            let val = std::str::from_utf8 (&i).unwrap();
+            Ok(Some(val.parse().unwrap()))
+        } else {
+            Ok(None)
+        }
+
+    }
+
+    fn remove(&mut self, key: String) -> Result<Option<String>> {
+        let resp = self.db.remove(key).unwrap();
+        if let Some(i) = resp {
+            Ok(Some("SUCCESS".to_string()))
+        } else {
+            Err(KvsError::KeyNotFound)
+        }
+    }
+}
+
+
 
 #[derive(Serialize, Deserialize)]
 #[derive(Debug)]
